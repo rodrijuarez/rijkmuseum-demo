274cde272b15dbce56e2b3dcbbedecc43fea11a7
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-30 19:55:13 -0300

    Enhancement - Added material-ui and appbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Head from '../components/head'
-import Link from 'next/link'
-import Nav from '../components/nav'
+import AppBar from 'material-ui/AppBar';
+import Head from '../components/head';
+import Link from 'next/link';
+import Nav from '../components/nav';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 export default () => (
   <div>
     <Head title="Home" />
     <Nav />
+    <AppBar>
+      <Toolbar>
+        <Typography type="title" color="inherit">
+          Rijksmuseum Demo
+        </Typography>
+      </Toolbar>
+    </AppBar>
   </div>
-)
+);
