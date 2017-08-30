db4d7da6f5553aba57421f1202d9ad06244d79c2
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-30 19:38:30 -0300

    Project initialization

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from '../components/head'
+import Link from 'next/link'
+import Nav from '../components/nav'
+
+export default () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
+  </div>
+)
